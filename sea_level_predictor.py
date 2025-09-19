a31ffb5ffeb15ee3b3939254702abfe128f4cdfb
Isabel Montenegro <isabel.ruela@sou.inteli.edu.br>
2025-09-19 13:28:24 -0300

    Implement sea level plot generation and regression lines
    Added code to read sea level data, generate a scatter plot, and plot two lines of best fit (one for all data and one for data since 2000). The plot is saved as 'sea_level_plot.png'.

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,23 +1,42 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
+
 def draw_plot():
-    # Read data from file
+    # Ler os dados do arquivo CSV
+    df = pd.read_csv('epa-sea-level.csv')
 
+    # Selecionar as colunas de interesse
+    anos = df['Year']
+    niveis_mar = df['CSIRO Adjusted Sea Level']
 
-    # Create scatter plot
+    # Criar o gráfico de dispersão
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.scatter(anos, niveis_mar)
 
+    # Primeira linha de regressão usando todos os dados
+    slope_all, intercept_all, r_all, p_all, std_err_all = linregress(anos, niveis_mar)
+    anos_ext = np.arange(anos.min(), 2051)
+    pred_all = intercept_all + slope_all * anos_ext
+    ax.plot(anos_ext, pred_all)
 
-    # Create first line of best fit
+    # Segunda linha de regressão usando apenas dados a partir de 2000
+    dados_recentes = df[df['Year'] >= 2000]
+    anos_recentes = dados_recentes['Year']
+    niveis_recentes = dados_recentes['CSIRO Adjusted Sea Level']
 
+    slope_rec, intercept_rec, r_rec, p_rec, std_err_rec = linregress(anos_recentes, niveis_recentes)
+    anos_rec_ext = np.arange(2000, 2051)
+    pred_rec = intercept_rec + slope_rec * anos_rec_ext
+    ax.plot(anos_rec_ext, pred_rec)
 
-    # Create second line of best fit
+    # Adicionar rótulos e título ao gráfico
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Sea Level (inches)')
+    ax.set_title('Rise in Sea Level')
 
-
-    # Add labels and title
-
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
+    # Salvar a imagem e retornar o objeto Axes
     plt.savefig('sea_level_plot.png')
-    return plt.gca()
+    return ax
